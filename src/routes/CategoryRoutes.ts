e881d47473cc063e9600615e0c3edaa8b6efceae
Adriel <adriel.fsantos@outlook.com>
2023-02-22 22:07:56 -0300

    Reordena rotas de CategoryRoutes

--- src/routes/CategoryRoutes.ts
+++ src/routes/CategoryRoutes.ts
@@ -5,10 +5,10 @@ const categoryRoutes = Router();
 
 categoryRoutes.get('/', categoryController.categories_list_get);
 
-categoryRoutes.get('/:id', categoryController.category_detail_get);
-
 categoryRoutes.get('/create', categoryController.category_create_get);
 categoryRoutes.post('/create', categoryController.category_create_post);
+
+categoryRoutes.get('/:id', categoryController.category_detail_get);
 
 categoryRoutes.get('/:id/update', categoryController.category_update_get);
 categoryRoutes.post('/:id/update', categoryController.category_update_post);
